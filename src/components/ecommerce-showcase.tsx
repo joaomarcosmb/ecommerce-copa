0f437ad91b15626af8e2ef71c38332d60fdc56f5
Joao Barroso <joao.barroso@alu.ufc.br>
2026-03-19 10:04:15 -0300

    feat: add breadcrumb

--- src/components/ecommerce-showcase.tsx
+++ src/components/ecommerce-showcase.tsx
@@ -4,6 +4,14 @@ import { ShowcaseHeader } from "@/components/ecommerce-showcase/header";
 import { ShowcaseHero } from "@/components/ecommerce-showcase/hero";
 import { InfoCarousel } from "@/components/ecommerce-showcase/info-carousel";
 import { ProductsSection } from "@/components/ecommerce-showcase/products";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export function EcommerceShowcase() {
   return (
@@ -11,6 +19,23 @@ export function EcommerceShowcase() {
       <ShowcaseHeader />
       <InfoCarousel />
       <main>
+        <section className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="#">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="#">Ecommerce</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Showcase</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </section>
         <ShowcaseHero />
         <ContentSections />
         <ProductsSection />
